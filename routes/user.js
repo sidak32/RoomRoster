b2bb0ee1d87692f3cba913aad0ec943d141ddf66
sidak32 <sidak2k4@gmail.com>
2025-07-11 10:19:21 +0530

    SignUp & Login Functionality

---
+++ routes/user.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
+
+router.get("/signup", (req, res) => {
+  res.render("users/signup.ejs");
+});
+
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      const newUser = new User({ email, username });
+      const registeredUser = await User.register(newUser, password);
+
+      req.login(registeredUser, (err) => {
+        if (err) {
+          return next(err);
+        }
+        req.flash("success", "Welcome to RoomRoster");
+        res.redirect("/listings");
+      });
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/signup");
+    }
+  })
+);
+
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  (req, res) => {
+    req.flash("success", "Welcome back to RoomRoster!");
+    res.redirect("/listings");
+  }
+);
+
+module.exports = router;
